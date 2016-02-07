4ff3fd51fa42b57dcd20da291f24c600c38af55c
Madelene Campos <madelenecampos@gmail.com>
2016-02-07 18:02:03 -0500

    Devise madness

--- db/migrate/20160207190401_create_admins.rb
+++ db/migrate/20160207190401_create_admins.rb
@@ -2,7 +2,7 @@ class CreateAdmins < ActiveRecord::Migration
   def change
     create_table :admins do |t|
       t.string :name
-      t.string :email
+    
 
       t.timestamps null: false
     end
